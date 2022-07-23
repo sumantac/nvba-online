c35a365fad4095b92fb24467e906519762a2f189
Dhruba Jyoti Dey <deydhrubajyoti@gmail.com>
2022-07-23 19:35:36 -0400

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
+import { TeamComponent } from './pages/aboutus/team/team.component';
 import { ContactusComponent } from './pages/contactus/contactus.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent, pathMatch: 'full' }, 
   { path: "home", component: HomeComponent },
+  { path: "team", component: TeamComponent, pathMatch: 'full'  },
   { path: "contactus", component: ContactusComponent, pathMatch: 'full'  },
 ];
 
--- src/app/pages/aboutus/team/team.component.ts
+++ src/app/pages/aboutus/team/team.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GetjsonfileService } from './../../../services/getjsonfile.service';
+
 
 @Component({
   selector: 'app-team',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./team.component.scss']
 })
 export class TeamComponent implements OnInit {
-
-  constructor() { }
+  nvbaTeam : any;
+  constructor(private jsonFile:GetjsonfileService) { }
 
   ngOnInit(): void {
+    this.jsonFile.pageData('ecm').subscribe(data => {
+         console.log(data);
+         this.nvbaTeam = data;
+       });
   }
 
 }
