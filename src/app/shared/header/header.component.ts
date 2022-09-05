41bf68ec3650f2e03045c1273d6cc6a131439e3a
Dhruba Jyoti Dey <deydhrubajyoti@gmail.com>
2022-09-05 16:38:57 -0400

    Member Details on header
    Member Details on header  and styling

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { GetjsonfileService } from './../../services/getjsonfile.service';
+import { AuthService } from './../services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -15,10 +16,13 @@ export class HeaderComponent implements OnInit {
   dataObject :any={};
   navbarCollapsed : any;
   isCollapsed = false;
+  member: any={}
 
   private _jsonURL = '/assets/data/pages/header.json';
 
-   constructor(private jsonFile:GetjsonfileService) {}
+   constructor(private jsonFile:GetjsonfileService, public auth:AuthService) {
+
+   }
 
 
   ngOnInit(): void {
@@ -33,6 +37,22 @@ export class HeaderComponent implements OnInit {
 
     element.click();
     console.log('click');
+  }
+
+  signout(){
+    this.auth.SignOut()
+    .then((res) => {
+      this.auth.cast.subscribe(m=> {
+        this.member = m;
+        console.log(this.member);
+      });
+   //   this.memberService.UpdateMember(this.member.id, this.member);
+  //    this.location.back();
+  //      localStorage.setItem('user', 'null');
+    }, (error) => {
+      console.log("Logout error", error);
+    });
+    this.toggle();
   }
 
 }
